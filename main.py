78a71ab3f239ea01bc2667669ef7265e6b84c188
AngelJeffersonJ <127641379+AngelJeffersonJ@users.noreply.github.com>
2025-09-02 13:54:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def obtener_saldo_recargaqui():
             with sync_playwright() as p:
                 browser = p.chromium.launch(headless=True, slow_mo=200)
                 page = browser.new_page()
-                page.goto("https://recargaqui.com.mx/vtae/")
+                page.goto("https://recargaqui.com.mx/vtae/login.aspx")
                 frame = None
                 for f in page.frames:
                     if "Login.aspx" in f.url:
@@ -217,5 +217,6 @@ if __name__ == "__main__":
             else:
                 print(f"Reintentando ciclo completo en 10 segundos... (Falla pagaqui={falla_pagaqui}, falla bait={falla_bait})\n")
                 time.sleep(10)
+
 
 
